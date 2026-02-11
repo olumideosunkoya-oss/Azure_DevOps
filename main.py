ab1748836f34d7421c8398c3ce1764289f393c69
olumideosunkoya-oss <olumide.osunkoya@gmail.com>
2026-02-11 15:57:54 +0000

    newuserfeatureadded

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello from Python"
+@app.route("/add")
+def add_user():
+    return "added a new user"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
